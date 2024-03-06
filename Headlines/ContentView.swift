bedb52b4e8b31850620fefc465c6058060c1f80c
Micah Napier <micah.napier@gmail.com>
2024-03-07 10:49:32 +1100

    initial commit

---
+++ Headlines/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Headlines
+//
+//  Created by Micah Napier on 7/3/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Headlines/HeadlinesApp.swift
@@ -0,0 +1,17 @@
+//
+//  HeadlinesApp.swift
+//  Headlines
+//
+//  Created by Micah Napier on 7/3/24.
+//
+
+import SwiftUI
+
+@main
+struct HeadlinesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
